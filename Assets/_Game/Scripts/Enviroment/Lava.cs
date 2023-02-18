6ca95f720691b7ca0e52dfd4367df339205dc0c3
IRKhabibullin <irkhabibullin@gmail.com>
2023-02-19 00:10:20 +0300

    urp lighting and lava glowing

--- Assets/_Game/Scripts/Enviroment/Lava.cs
+++ Assets/_Game/Scripts/Enviroment/Lava.cs
@@ -6,6 +6,42 @@ using UnityEngine;
 
 public class Lava : MonoBehaviour
 {
+    [Range(0,5)][SerializeField] private float glowIntensity;
+    [SerializeField] private float glowUpTime;
+
+    private Material _material;
+    private float currentGlowIntensity;
+    private bool glowingUp = true;
+    private static readonly int GlowIntensity = Shader.PropertyToID("_GlowIntensity");
+
+    private void Awake()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if (glowingUp)
+        {
+            currentGlowIntensity += glowIntensity * Time.deltaTime / glowUpTime;
+
+            if (currentGlowIntensity >= glowIntensity)
+            {
+                glowingUp = false;
+            }
+        }
+        else
+        {
+            currentGlowIntensity -= glowIntensity * Time.deltaTime / glowUpTime;
+
+            if (currentGlowIntensity <= 0)
+            {
+                glowingUp = true;
+            }
+        }
+        _material.SetFloat(GlowIntensity, currentGlowIntensity);
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
